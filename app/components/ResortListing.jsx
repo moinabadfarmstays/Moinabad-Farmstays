f98106dff9a3a4bf081eb8a7d6d57c9f47baff1c
KADUMU0980 <srisaiteja384@gmail.com>
2026-07-04 12:02:46 +0530

    Harden sitemap generation against DB timeouts for Google fetch

--- app/components/ResortListing.jsx
+++ app/components/ResortListing.jsx
@@ -1,10 +1,7 @@
-import SiteLayout from "./layout/SiteLayout";
 import ProductCollection from "./ProductCollection";
 
 export default async function ResortListing({ initialSearch = "" }) {
   return (
-    <SiteLayout>
-      <ProductCollection initialSearch={initialSearch} />
-    </SiteLayout>
+    <ProductCollection initialSearch={initialSearch} />
   );
 }
